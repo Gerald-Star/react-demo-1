ed58905edb5162ddcee712b14dce3e847ef2c5e0
Gerald-Star <ukwuomagerald@gmail.com>
2022-05-26 01:47:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+
+// export default function App() {
+//   return (
+//     <div>
+
+//       <h1>My name is Chima</h1>
+//       <h2>This is my year</h2>
+//     </div>
+//   )
+// }
+
+
+export default function Menu() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h3>Click&Pay</h3>
+        <img src="./logo192.png" alt="" width="30px" />
+
+        <nav>
+          <ul>
+            <li>Women</li>
+            <li>Men</li>
+            <li>Kids</li>
+          </ul>
+        </nav>
       </header>
+      
+      <h1>Your one stop online shop</h1>      
+      
+      <footer>
+        <small>2021 Zirol development. All rights reserved.</small>
+      </footer>
     </div>
-  );
+    
+    
+  )
 }
 
-export default App;
+// function Page() {
+//   return (
+//     <div>
+//       <h3>why I like react</h3>
+//     </div>
+//   )
+// }
+  
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+import Menu from './App';
+// import Page from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const page = ReactDOM.createRoot(document.getElementById('root'));
+// page.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const home = ReactDOM.createRoot(document.getElementById('root'));
+
+home.render(
   <React.StrictMode>
-    <App />
+    <Menu />
+    {/* <img src="./logo192.png" alt=""/> */}
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const page = ReactDOM.createRoot(document.getElementById('root'));
+
+// page.render(
+//   <React.StrictMode>
+//     <Page />
+//   </React.StrictMode>
+
+// )
+
+
+
